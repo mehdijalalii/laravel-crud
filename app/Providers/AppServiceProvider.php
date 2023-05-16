9d4bcac430a37b2f740c42e94acdb8d8f2cf0a39
mehdi <mehdi.jalalii03@gmail.com>
2023-05-17 02:42:17 +0330

    [feat] Add AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,14 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Src\Shared\Domain\Bus\Command\CommandBusInterface;
+use Src\Shared\Domain\Bus\Event\EventBusInterface;
+use Src\Shared\Domain\Bus\Query\QueryBusInterface;
+use Src\Shared\Domain\UuidGeneratorInterface;
+use Src\Shared\Infrastructure\Bus\Messenger\MessengerCommandBus;
+use Src\Shared\Infrastructure\Bus\Messenger\MessengerEventBus;
+use Src\Shared\Infrastructure\Bus\Messenger\MessengerQueryBus;
+use Src\Shared\Infrastructure\RamseyUuidGenerator;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +19,33 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(
+            EventBusInterface::class,
+            function ($app) {
+                return new MessengerEventBus($app->tagged('domain_event_subscriber'));
+            }
+        );
+
+        $this->app->bind(
+            QueryBusInterface::class,
+            function ($app) {
+                return new MessengerQueryBus($app->tagged('query_handler'));
+            }
+        );
+
+        $this->app->bind(
+            CommandBusInterface::class,
+            function ($app) {
+                return new MessengerCommandBus($app->tagged('command_handler'));
+            }
+        );
+
+        $this->app->bind(
+            UuidGeneratorInterface::class,
+            RamseyUuidGenerator::class
+        );
+
+
     }
 
     /**
